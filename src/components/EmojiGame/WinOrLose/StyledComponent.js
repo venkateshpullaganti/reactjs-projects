6e8d830bd583eb9651a77a309e03a7f7bcc38882
Venky <venkatesh6123.pv@gmail.com>
2020-04-04 16:04:36 +0000

    upda ipad

--- src/components/EmojiGame/WinOrLose/StyledComponent.js
+++ src/components/EmojiGame/WinOrLose/StyledComponent.js
@@ -7,26 +7,29 @@ import { css } from "@emotion/core";
 
 
 const dynamicStyle = props =>
-    css` 
+    css ` 
     color:${props.selectedTheme.color};
     background:${props.selectedTheme.backgroundColor};
     `;
-
+const fullHeight = props =>
+    css `
+    height:95%;
+`;
 const statColor = props =>
-    css` color :${props.isWon ? "green" : "red"}`;
+    css ` color :${props.isWon ? "green" : "red"}`;
 
-const ResultDiv = styled.div`
+const ResultDiv = styled.div `
     
-    ${tw`flex flex-grow flex-col items-center justify-center p-4    border-solid border-4 border-green-600`}
+    ${tw`flex flex-grow h-full flex-col items-center justify-center p-4 border-solid border-4 border-green-600`}
 `;
 
-const FinalScore = styled.p`${tw`font-bold text-2xl `}`;
+const FinalScore = styled.p `${tw`font-bold text-2xl `}`;
 
-const GameResult = styled.p`
+const GameResult = styled.p `
 ${statColor}
 ${tw`font-bold text-2xl`} `;
 
-const PlayAgainBtn = styled.button`${tw`text-white-200 text-2xl bg-blue-700 p-4 rounded`};
+const PlayAgainBtn = styled.button `${tw`text-white-200 text-2xl bg-blue-700 p-4 rounded`};
  &:focus{
     outline:none
 } `;
