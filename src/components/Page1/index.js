7364f5d2a1f32ada92c7ff068d26817a8b2ff0cb
venakteshpullaganti <venkatesh6123.pv@gmail.com>
2020-04-11 12:36:26 +0530

    inject

--- src/components/Page1/index.js
+++ src/components/Page1/index.js
@@ -2,7 +2,8 @@
 import tw from "tailwind.macro";
 import { css, jsx } from "@emotion/core";
 import React from "react";
-import { reactions, autorun, observable, computed } from "mobx";
+import { reactions, autorun, observable, computed, observe } from "mobx";
+
 
 
 // import { cx } from "emotion";
@@ -17,152 +18,222 @@ import { reactions, autorun, observable, computed } from "mobx";
 
 import styled from '@emotion/styled'
 
-import logo from "../../logo.svg";
+// import logo from "../../logo.svg";
+import { Provider, inject, observer } from "mobx-react";
 
-const Pagediv = styled.div`
+// const Pagediv = styled.div`
 
-    ${tw('h-screen bg-gray-800 flex justify-center items-center flex-col')}
-`;
+//     ${tw('h-screen bg-gray-800 flex justify-center items-center flex-col')}
+// `;
 
-const StyledBtn = styled.button`
-    height:50px;
-    widht:100px;
-    color : green;
-    background-color:grey
+// const StyledBtn = styled.button`
+//     height:50px;
+//     widht:100px;
+//     color : green;
+//     background-color:grey
+// `
+
+
+// const StyledBtn2 = styled.button`
+//     widht:100px;
+//     height:100px;
+//     font-weight:bold;
+//     background:${props =>
+//         props.Selectedtheme.name === "light" ? "white" : "red"
+//     };
+//     color:${props =>
+//         props.Selectedtheme.name === "light" ? "black" : "white"
+//     };
+// `
+
+// const ThemedDiv = styled.div(
+//     {
+//         height: "100px",
+//         width: "100px",
+//         border: "1px solid pink",
+//         bordeRadius: "3px"
+
+//     },
+//     props => ({ color: props.selectedTheme.color, backgroundColor: props.selectedTheme.background }))
+
+
+const DivStyled = styled.div`
+    padding:50px;
+    border:2px solid green;
 `
 
 
-const StyledBtn2 = styled.button`
-    widht:100px;
-    height:100px;
-    font-weight:bold;
-    background:${props =>
-        props.Selectedtheme.name === "light" ? "white" : "red"
-    };
-    color:${props =>
-        props.Selectedtheme.name === "light" ? "black" : "white"
-    };
-`
+@inject("temp")
+@observer
+class A extends React.Component {
 
-const ThemedDiv = styled.div(
-    {
-        height: "100px",
-        width: "100px",
-        border: "1px solid pink",
-        bordeRadius: "3px"
+    @observable name = "";
 
-    },
-    props => ({ color: props.selectedTheme.color, backgroundColor: props.selectedTheme.background }))
-
-
-
-class Page1 extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedTheme: "light"
-        }
-
-
-        this.themeOptions = {
-            light: {
-                id: 0,
-                key: "light",
-                name: "light theme",
-                color: "black",
-                background: "white"
-            },
-            dark: {
-                id: 1,
-                key: "dark",
-                name: "dark theme",
-                color: "white",
-                background: "black"
-            },
-            monaki: {
-                id: 2,
-                key: "monaki",
-                name: "monaki",
-                color: "purple",
-                background: "lightgrey"
-            }
-        }
-
-    }
-    changeTheme = (e) => {
-        this.setState({ selectedTheme: e.target.value });
+    onChange = (event) => {
+        this.name = event.target.value;
     }
 
+    render() {
+        console.log("A", this.props.temp);
+        return (
+            <DivStyled>
+                <input style={{ background: "green" }} defaultValue={this.name} onChange={this.onChange} />
+                <B name={this.name} />
+            </DivStyled>
+        )
+    }
+}
 
+
+@inject("temp")
+class B extends React.Component {
 
 
     render() {
+        console.log("B", this.props.temp, this.props.name);
         return (
-            <Pagediv >
-                <img src={logo} className="App-logo" alt="logo" />
-                <p className="text-white">Page 1</p>
+            <DivStyled>
 
-                <div className={css`   /* using the raw css in emotion */
-                height:100px;       /*this doesn't mean an inline-css */
-                width:100px;
-                font-size:20px;                 
-                border-radius:4px;
-                background-color:purple;
-                &:hover{
-                    color:${this.color};
-                }
-            `
+                <C />
 
-                }>
-                    emotion.
-            </div>
-
-
-
-                <div css={{
-                    backgroundColor: this.themeOptions[this.state.selectedTheme].background,   /*css props by object styling*/
-                    color: this.themeOptions[this.state.selectedTheme].color,
-                    height: "100px",
-                    width: "100px",
-                    '&:hover': {
-                        color: "white"
-                    }
-
-                }}
-                    className="border"
-                >Css props - object style </div>
-
-                <div css={css`
-            background-color: yellow; /* css props by string style*/
-            height:100px;
-            width:100px;
-            &:hover{
-                color:${this.color};
-            }
-            `}>
-                    Css props - string style.
-            </div>
-                <div className="flex flex-col h-20">
-                    <form>
-                        <StyledBtn type="button">styled component</StyledBtn>
-
-                        <StyledBtn2 Selectedtheme={{ name: "dark" }} onClick={this.changeTheme} >themed Button</StyledBtn2>
-
-                        <select value={this.state.selectedTheme} onChange={this.changeTheme}>
-                            {
-                                Object.keys(this.themeOptions).map(theme =>
-                                    <option key={theme} value={theme}>{theme}</option>
-                                )
-                            }
-                        </select>
-
-                    </form>
-                    <ThemedDiv selectedTheme={this.themeOptions[this.state.selectedTheme]} >ThemedDiv</ThemedDiv>
-                </div>
-            </Pagediv>
-
-        );
+            </DivStyled>
+        )
     }
+}
+
+@inject("temp")
+class C extends React.Component {
+
+
+    render() {
+        console.log("C", this.props.temp);
+        return (
+            <p>THis is C comp.</p>
+
+        )
+    }
+}
+
+
+class Page1 extends React.Component {
+    render() {
+        return (
+            <DivStyled>
+
+                <Provider temp={"Venky"}>
+                    <A temp={"from parent"} />
+                </Provider>
+
+            </DivStyled>
+        )
+    }
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         selectedTheme: "light"
+    //     }
+
+
+    //     this.themeOptions = {
+    //         light: {
+    //             id: 0,
+    //             key: "light",
+    //             name: "light theme",
+    //             color: "black",
+    //             background: "white"
+    //         },
+    //         dark: {
+    //             id: 1,
+    //             key: "dark",
+    //             name: "dark theme",
+    //             color: "white",
+    //             background: "black"
+    //         },
+    //         monaki: {
+    //             id: 2,
+    //             key: "monaki",
+    //             name: "monaki",
+    //             color: "purple",
+    //             background: "lightgrey"
+    //         }
+    //     }
+
+    // }
+    // changeTheme = (e) => {
+    //     this.setState({ selectedTheme: e.target.value });
+    // }
+
+
+
+
+    // render() {
+    //     return (
+    //         <Pagediv >
+    //             <img src={logo} className="App-logo" alt="logo" />
+    //             <p className="text-white">Page 1</p>
+
+    //             <div className={css`   /* using the raw css in emotion */
+    //             height:100px;       /*this doesn't mean an inline-css */
+    //             width:100px;
+    //             font-size:20px;                 
+    //             border-radius:4px;
+    //             background-color:purple;
+    //             &:hover{
+    //                 color:${this.color};
+    //             }
+    //         `
+
+    //             }>
+    //                 emotion.
+    //         </div>
+
+
+
+    //             <div css={{
+    //                 backgroundColor: this.themeOptions[this.state.selectedTheme].background,   /*css props by object styling*/
+    //                 color: this.themeOptions[this.state.selectedTheme].color,
+    //                 height: "100px",
+    //                 width: "100px",
+    //                 '&:hover': {
+    //                     color: "white"
+    //                 }
+
+    //             }}
+    //                 className="border"
+    //             >Css props - object style </div>
+
+    //             <div css={css`
+    //         background-color: yellow; /* css props by string style*/
+    //         height:100px;
+    //         width:100px;
+    //         &:hover{
+    //             color:${this.color};
+    //         }
+    //         `}>
+    //                 Css props - string style.
+    //         </div>
+    //             <div className="flex flex-col h-20">
+    //                 <form>
+    //                     <StyledBtn type="button">styled component</StyledBtn>
+
+    //                     <StyledBtn2 Selectedtheme={{ name: "dark" }} onClick={this.changeTheme} >themed Button</StyledBtn2>
+
+    //                     <select value={this.state.selectedTheme} onChange={this.changeTheme}>
+    //                         {
+    //                             Object.keys(this.themeOptions).map(theme =>
+    //                                 <option key={theme} value={theme}>{theme}</option>
+    //                             )
+    //                         }
+    //                     </select>
+
+    //                 </form>
+    //                 <ThemedDiv selectedTheme={this.themeOptions[this.state.selectedTheme]} >ThemedDiv</ThemedDiv>
+    //             </div>
+    //         </Pagediv>
+
+    //     );
+    // }
 }
 
 export default Page1;
