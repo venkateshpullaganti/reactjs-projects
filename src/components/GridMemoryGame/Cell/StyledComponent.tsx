5003b3ee9f7715cfc72de23e7c87233266b108ab
Venky <38013987+venkateshpullaganti@users.noreply.github.com>
2020-04-17 10:22:25 +0530

    upd

--- src/components/GridMemoryGame/Cell/StyledComponent.tsx
+++ src/components/GridMemoryGame/Cell/StyledComponent.tsx
@@ -11,11 +11,23 @@ type PropsType = {
 const CellStyled = styled.button`
 width: ${ (props: PropsType) => props.width - 8 + 'px'};    //decreasing the cell size for the margin
 height:${ (props: PropsType) => props.width - 8 + 'px'};
-background:${(props: PropsType) => props.background};
-margin:4px;
-transition: backgroundColor 2s;
-outline:none;
+background-image:${(props: PropsType) => `linear-gradient(${props.background},${props.background})`};
+
+background-repeat: no-repeat;
+margin: 4px;
+transition: background-size .5s;
+&: focus{
+    outline: none;
+};
 `;
 
 
 export { CellStyled };
+
+// border:1px solid white;
+// background-size: 0% 0%;
+// background-repeat: no-repeat;
+// background-position: 50% 50%;
+// background-size: 100% 100%;
+
+
